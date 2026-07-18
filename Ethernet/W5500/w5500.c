fd9397026b00f50a012c9680588261cd7e3d80ff
Abe Kabakoff <occamsshavingkit@gmail.com>
2026-07-18 20:58:19 +0200

    fix: add len==0 early return to wiz_recv_ignore (AUD-047)

--- Ethernet/W5500/w5500.c
+++ Ethernet/W5500/w5500.c
@@ -240,6 +240,7 @@ void wiz_recv_data(uint8_t sn, uint8_t *wizdata, uint16_t len) {
 void wiz_recv_ignore(uint8_t sn, uint16_t len) {
     uint16_t ptr = 0;
 
+    if (len == 0) return;
     ptr = getSn_RX_RD(sn);
     ptr += len;
     setSn_RX_RD(sn, ptr);
